ce3251c75832e65bd32c97c5c2f17a79f90561e1
maksim.ku <maksim.ku@onyxenterprises.com>
2019-11-24 14:48:38 +0200

    order now button check for disable

--- src/containers/burgerbuilder/burgerbuilder.js
+++ src/containers/burgerbuilder/burgerbuilder.js
@@ -7,7 +7,7 @@ const INGREDIENT_PRICES = {
     cheese: 0.4,
     meat: 1.3,
     bacon: 0.7,
-}
+};
 export default class BurgerBuilder extends Component {
 /*    constructor (props) {
         super(props)
@@ -21,6 +21,17 @@ export default class BurgerBuilder extends Component {
             meat: 0
         },
         totalPrice: 4,
+        purchasable: false,
+    };
+    updatePurchaseState = (ingredients) => {
+        const sum = Object.keys(ingredients)
+                .map(igKey => {
+                    return ingredients[igKey];
+                })
+                .reduce((sum, el) => {
+                    return sum + el;
+                }, 0);
+        this.setState({purchasable: sum > 0})
     };
 
     addIngredientHandler = (type) => {
@@ -36,7 +47,8 @@ export default class BurgerBuilder extends Component {
         this.setState({
             totalPrice: newPrice,
             ingredients: updatedIngredients
-        })
+        });
+        this.updatePurchaseState(updatedIngredients);
     };
 
     removeIngredientHandler = (type) => {
@@ -56,6 +68,7 @@ export default class BurgerBuilder extends Component {
             totalPrice: newPrice,
             ingredients: updatedIngredients
         });
+        this.updatePurchaseState(updatedIngredients);
     };
 
     render () {
@@ -74,6 +87,7 @@ export default class BurgerBuilder extends Component {
                     ingredientRemoved = {this.removeIngredientHandler}
                     disabled = {disabledInfo}
                     price={this.state.totalPrice}
+                    purchasable = {this.state.purchasable}
                 />
             </Auxx>
         );
