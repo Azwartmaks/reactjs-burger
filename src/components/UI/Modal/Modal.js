455abeb2b013b283a2306872207a3aab6423c622
maksim.ku <maksim.ku@onyxenterprises.com>
2019-11-29 08:50:23 +0200

    toolbar nav + performance for modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Auxx from "../../../hoc/Auxx";
 import Backdrop from "../Backdrop/Backdrop";
 
 import classes from './Modal.css';
-const modal = (props) => (
-    <Auxx>
-        <Backdrop clicked={props.cancel} show={props.show}/>
-        <div className={classes.Modal}
-             style = {{
-                 transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                 opacity: props.show ? '1' : '0'
-             }}>
-            {props.children}
-        </div>
-    </Auxx>
 
-);
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        return nextProps.show !== this.props.show;
+    }
 
-export default modal;
+    render() {
+        return (
+            <Auxx>
+                <Backdrop clicked={this.props.cancel} show={this.props.show}/>
+                <div className={classes.Modal}
+                     style = {{
+                         transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                         opacity: this.props.show ? '1' : '0'
+                     }}>
+                    {this.props.children}
+                </div>
+            </Auxx>
+        )
+    }
+};
+
+export default Modal;
